0c479da9b172879d49deed96a4e4704652755b1a
augustocastro <augusto1castro@gmail.com>
2019-07-31 21:23:13 -0300

    Corrigindo a treta do CORS.

--- src/main/java/br/com/infobtc/config/cors/WebConfig.java
+++ src/main/java/br/com/infobtc/config/cors/WebConfig.java
@@ -12,9 +12,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
     	registry.addMapping("/**")
-//      .allowedOrigins("http://localhost:4200")
+    	.allowedOrigins("*")
         .allowedMethods("*")
-        .allowedHeaders("*");
+        .allowedHeaders("*")
+        .allowCredentials(false);
     }
     
 }
