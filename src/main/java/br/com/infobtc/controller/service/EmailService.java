de9a462088b2078119ff4d50722ab053a1210d82
augustocastro <augusto1castro@gmail.com>
2019-08-08 12:55:38 +0000

    Update EmailService.java

--- src/main/java/br/com/infobtc/controller/service/EmailService.java
+++ src/main/java/br/com/infobtc/controller/service/EmailService.java
@@ -14,9 +14,9 @@ public class EmailService {
 	public void send(String ...args) {
 		try {
 			StringBuilder msg = new StringBuilder();
-			msg.append("<html><p>Olá, %s. Você acaba de receber um token para realizar o seu cadastro em nosso sistema. O código é: <strong>%s</strong></html>.");
+			msg.append("<html><p>Olá, %s. Você acaba de receber um token para realizar o seu cadastro em nosso sistema. O código é: <strong>%s</strong>.");
 			msg.append("<br>Faça o cadastro até às 00:00 do dia de hoje, pois token expira o nesse horário.");
-			msg.append("<br><br>Para se cadastrar acesso agora: http://infobtcbr.com.br/cliente.</p>");
+			msg.append("<br><br>Para se cadastrar acesse agora: http://infobtcbr.com.br/cliente.</p></html>");
 			
 			String mensagem = String.format(msg.toString(), args[0], args[2]);
 			
