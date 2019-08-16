acc2e4c66174fce6d002afa86a03696eb4f04e97
augustocastro <augusto1castro@gmail.com>
2019-08-16 14:13:30 +0000

    Update BancoDto.java

--- src/main/java/br/com/infobtc/controller/dto/BancoDto.java
+++ src/main/java/br/com/infobtc/controller/dto/BancoDto.java
@@ -4,7 +4,7 @@ import br.com.infobtc.model.Banco;
 
 public class BancoDto {
 
-	private String instituicao_finaceira;
+	private String instituicao_financeira;
 	private String agencia;
 	private String conta;
 	private String tipo_conta;
@@ -13,7 +13,7 @@ public class BancoDto {
 	private String cpf_or_cnpj_titular;
 
 	public BancoDto(Banco banco) {
-		this.instituicao_finaceira = banco.getInstituicaoFinaceira();
+		this.instituicao_financeira = banco.getInstituicaoFinanceira();
 		this.agencia = banco.getAgencia();
 		this.conta = banco.getConta();
 		this.tipo_conta = banco.getTipoConta();
