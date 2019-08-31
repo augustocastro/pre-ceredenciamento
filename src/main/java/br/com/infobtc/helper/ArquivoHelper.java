825acb7d84fc3dedf728c7cfa66f5269cee185ba
augustocastro <augusto1castro@gmail.com>
2019-08-31 11:07:24 -0300

    Correções e refatoração.

---
+++ src/main/java/br/com/infobtc/helper/ArquivoHelper.java
@@ -0,0 +1,30 @@
+package br.com.infobtc.helper;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ArquivoHelper {
+
+	private static final String CAMINHO_PASTA_RESOURCE = "src/main/resources/";
+
+	public static String lerArquivo(String arquvivo) throws IOException {
+		String file = CAMINHO_PASTA_RESOURCE + arquvivo;
+		StringBuilder conteudo = new StringBuilder();
+		
+		FileReader ler = new FileReader(file);
+		BufferedReader reader = new BufferedReader(ler);
+		
+		String linha;
+
+		while ((linha = reader.readLine()) != null) {
+			conteudo.append(linha);
+		}
+
+		if (reader != null) {
+			reader.close();
+		}
+
+		return conteudo.toString();
+	}
+}
--- src/main/java/br/com/infobtc/service/ContratoPDFService.java
+++ src/main/java/br/com/infobtc/service/ContratoPDFService.java
@@ -1,9 +1,10 @@
 package br.com.infobtc.service;
 
-import java.io.BufferedReader;
+import static br.com.infobtc.helper.ArquivoHelper.lerArquivo;
+import static br.com.infobtc.helper.DataHelper.retornaMes;
+
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.time.LocalDate;
@@ -25,41 +26,32 @@ import br.com.infobtc.model.InvestidorPessoaJuridica;
 @Service
 public class ContratoPDFService {
 
-	private final String CAMINHO_PASTA_RESOURCE = "src/main/resources/contrato";
-
 	public File gerarPdf(Contrato contrato) throws IOException, DocumentException {
 		OutputStream os = null;
-
-		boolean isInvestimento = contrato instanceof ContratoInvestimento ? true : false;
 		
+		boolean isInvestimento = contrato instanceof ContratoInvestimento ? true : false;
 		Map<String, String> dadosInvestidor = retornaDadosInvestidor(contrato, isInvestimento);
 		LocalDate hoje = LocalDate.now();
+		
+		String nomeInvestidor = dadosInvestidor.get("nomeInvestidor");
+		String naoSei1 = dadosInvestidor.get("naoSe1");
+		String naoSei2 = dadosInvestidor.get("naoSe2");
+		String tipoDocumento = dadosInvestidor.get("tipoDocumento");
+		String documento = dadosInvestidor.get("documento");
+		String endereco = dadosInvestidor.get("endereco");
+		String cep = dadosInvestidor.get("cep");
 
-		String page1 = String.format(lerArquivo("page1.txt"), 
-				dadosInvestidor.get("nomeInvestidor"),
-				dadosInvestidor.get("NAO_SEI1"), 
-				dadosInvestidor.get("NAO_SEI1"), 
-				dadosInvestidor.get("tipoDocumento"),
-				dadosInvestidor.get("documento"), 
-				dadosInvestidor.get("endereco"), 
-				dadosInvestidor.get("cep"), 
-				"10%");
-
-		String page2 = lerArquivo("page2.txt");
-		String page3 = lerArquivo("page3.txt");
-
-		String page4 = String.format(lerArquivo("page4.txt"), 
-				hoje.getDayOfMonth(), 
-				retornaMes(hoje.getMonthValue()),
-				hoje.getYear(), 
-				dadosInvestidor.get("nomeInvestidor"), 
-				dadosInvestidor.get("tipoDocumento"),
-				dadosInvestidor.get("documento"));
+		String page1 = String.format(lerArquivo("contrato/page1.txt"), nomeInvestidor, naoSei1, naoSei2, tipoDocumento, 
+				documento, endereco, cep, "10%");
+		String page2 = lerArquivo("contrato/page2.txt");
+		String page3 = lerArquivo("contrato/page3.txt");
+		String page4 = String.format(lerArquivo("contrato/page4.txt"), hoje.getDayOfMonth(), retornaMes(hoje.getMonthValue()), 
+				hoje.getYear(), nomeInvestidor, tipoDocumento, documento);
 
 		try {
 			final String[] paginas = new String[] { page1, page2, page3, page4 };
 
-			final File outputFile = File.createTempFile("FlyingSacuer.PDFRenderToMultiplePages", ".pdf");
+			final File outputFile = File.createTempFile("contrato", ".pdf");
 			os = new FileOutputStream(outputFile);
 
 			ITextRenderer renderer = new ITextRenderer();
@@ -84,41 +76,22 @@ public class ContratoPDFService {
 		}
 	}
 
-	private String lerArquivo(String nomeArquivo) throws IOException {
-		String file = CAMINHO_PASTA_RESOURCE + "/" + nomeArquivo;
-		StringBuilder conteudo = new StringBuilder();
-		FileReader ler = new FileReader(file);
-		BufferedReader reader = new BufferedReader(ler);
-		String linha;
-
-		while ((linha = reader.readLine()) != null) {
-			conteudo.append(linha);
-		}
-		
-		if (reader != null) {
-			reader.close();
-		}
-
-		return conteudo.toString();
-	}
-
 	private Map<String, String> retornaDadosInvestidor(Contrato contrato, boolean isInvestimento) {
 		Map<String, String> dadosInvestidor = new HashMap<String, String>();
+		
 		String documento;
 		String tipoDocumento;
-		
+
 		if (isInvestimento) {
 			ContratoInvestimento contratoInvestimento = (ContratoInvestimento) contrato;
 
 			if (contratoInvestimento.getInvestidor().getTipo().equals("pessoa_fisica")) {
-				InvestidorPessoaFisica investidorPessoaFisica = (InvestidorPessoaFisica) contratoInvestimento
-						.getInvestidor();
+				InvestidorPessoaFisica investidorPessoaFisica = (InvestidorPessoaFisica) contratoInvestimento.getInvestidor();
 				documento = investidorPessoaFisica.getCpf();
 				tipoDocumento = "CPF";
 			} else {
-				InvestidorPessoaJuridica investidorPessoaFisica = (InvestidorPessoaJuridica) contratoInvestimento
-						.getInvestidor();
-				documento = investidorPessoaFisica.getCnpj();
+				InvestidorPessoaJuridica investidorPessoaJuridica = (InvestidorPessoaJuridica) contratoInvestimento.getInvestidor();
+				documento = investidorPessoaJuridica.getCnpj();
 				tipoDocumento = "CNPJ";
 			}
 
@@ -127,8 +100,8 @@ public class ContratoPDFService {
 			dadosInvestidor.put("documento", documento);
 			dadosInvestidor.put("endereco", contratoInvestimento.getInvestidor().getEndereco().getEndereco().toUpperCase());
 			dadosInvestidor.put("cep", contratoInvestimento.getInvestidor().getEndereco().getCep());
-			dadosInvestidor.put("NAO_SEI1", "NAO_SEI1");
-			dadosInvestidor.put("NAO_SEI2", "NAO_SEI2");
+			dadosInvestidor.put("naoSe1", "Não Sei 1");
+			dadosInvestidor.put("naoSe2", "Não Sei 2");
 		} else {
 			ContratoReinvestimento contratoReinvestimento = (ContratoReinvestimento) contrato;
 			Investidor investidor = contratoReinvestimento.getInvestimento().getInvestidor();
@@ -138,8 +111,8 @@ public class ContratoPDFService {
 				documento = investidorPessoaFisica.getCpf();
 				tipoDocumento = "CPF";
 			} else {
-				InvestidorPessoaJuridica investidorPessoaFisica = (InvestidorPessoaJuridica) investidor;
-				documento = investidorPessoaFisica.getCnpj();
+				InvestidorPessoaJuridica investidorPessoaJuridica = (InvestidorPessoaJuridica) investidor;
+				documento = investidorPessoaJuridica.getCnpj();
 				tipoDocumento = "CNPJ";
 			}
 
@@ -148,42 +121,11 @@ public class ContratoPDFService {
 			dadosInvestidor.put("documento", documento);
 			dadosInvestidor.put("endereco", investidor.getEndereco().getEndereco().toUpperCase());
 			dadosInvestidor.put("cep", investidor.getEndereco().getCep());
-			dadosInvestidor.put("NAO_SEI1", "NAO_SEI1");
-			dadosInvestidor.put("NAO_SEI2", "NAO_SEI2");
+			dadosInvestidor.put("naoSe1", "Não Sei 1");
+			dadosInvestidor.put("naoSe2", "Não Sei 2");
 		}
 
 		return dadosInvestidor;
-	}
-
-	private String retornaMes(int mes) {
-		switch (mes) {
-		case 1:
-			return "Janeiro";
-		case 2:
-			return "Fevereiro";
-		case 3:
-			return "Março";
-		case 4:
-			return "Abril";
-		case 5:
-			return "Maio";
-		case 6:
-			return "Junho";
-		case 7:
-			return "Julho";
-		case 8:
-			return "Agosto";
-		case 9:
-			return "Setembro";
-		case 10:
-			return "Outubro";
-		case 11:
-			return "Novembro";
-		case 12:
-			return "Dezembro";
-		default:
-			return "";
-		}
 	}
 
 }
